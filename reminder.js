5dc98e281d2d58b8a741822d2bb9f61b7614b0a1
mohsin6862 <mohsinctg2206@gmail.com>
2023-02-03 20:17:35 +0600

    solution added

---
+++ reminder.js
@@ -0,0 +1,7 @@
+function findRemainder (mod){
+
+    let div = 5;
+    let reminder = mod % div;
+    console.log(reminder);
+    }
+    findRemainder(119);
---
+++ stringOperation.js
@@ -0,0 +1,8 @@
+function stringCombine(str1, str2){
+
+
+    var finalOutput = str1 + " " +str2;
+    console.log(finalOutput);
+    }
+
+    stringCombine( "i am a", "web developer");
